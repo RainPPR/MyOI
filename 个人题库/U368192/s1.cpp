5aee57aba074aa8b3d7a44ce3522d1fca4debd8f
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-07-07 19:59:06 +0800

    Commit

--- 个人题库/U368192/s1.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-signed main() {
-    long long a, b, e; int k, c;
-    scanf("%lld %lld %d", &a, &b, &k);
-    e = a ^ b, c = 0; while (e) e -= e & -e, ++c;
-    printf(c == k ? "Yes\n" : "No\n");
-    return 0;
-}
